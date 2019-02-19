64fdf9699dc4f0850c571dacf45de9caf9d6a1ab
lfresco <43806408+lfresco@users.noreply.github.com>
2019-02-19 10:31:58 +0100

    Third Exercise with Testing

---
+++ my_exercises/python/03_advanced/test.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 09:39:57 2019
+
+@author: ziki
+"""
+
+import unittest
+
+def myRange(arg1, arg2 = None, step =1):
+    
+    if arg1 == None:
+        raise TypeError('Should at least have an arg')
+    addList=[]
+    if arg2 != None:
+        begin = arg1
+        end = arg2
+    else:
+        begin = 0
+        end = arg1
+    i = begin
+    while(i != end):
+        addList.append(i)
+        i= i + step
+    return addList
+        
+        
+    
+class testmyRange(unittest.TestCase):
+    
+    def test_min(self):
+        with self.assertRaises(TypeError):
+            myRange()
+
+    def test_equality(self):
+        
+        self.assertEqual(myRange(1, 11), myRange(1,11))
+        self.assertEqual(myRange(21, 0, -3), myRange(21, 0, -3))
+
+
+    def test_dimension(self):
+        self.assertEqual(100, len(myRange(100)))
+
+if __name__ == '__main__':
+    unittest.main()
+      
